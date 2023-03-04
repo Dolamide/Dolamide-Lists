6c647c4c9c3aa97229987605f4d47fa47d5e60c1
Deborah Campbell <deborahcampbell@yahoo.com>
2023-03-04 14:29:21 +0000

    Coding Lab: Lists

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,7 +30,8 @@ public class Lab {
      * @return a List<Integer> object.
      */
     public List<Integer> createList(){
-        return null;
+        List<Integer>list = new ArrayList<Integer>();
+        return list; 
     }
 
     /**
@@ -39,7 +41,7 @@ public class Lab {
      * @return the size of List (number of items it holds.)
      */
     public int getSize(List<Integer> list){
-        return 0;
+        return list.size();
     }
 
     /**
@@ -51,6 +53,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addToList(List<Integer> list, int value){
+        list.add(value);
     }
 
     /**
